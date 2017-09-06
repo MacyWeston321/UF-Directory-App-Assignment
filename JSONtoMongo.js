93cb73d885713bd0598f08c7c7437df2077c5d9d
Macy Weston <macyweston@ufl.edu>
2017-09-06 18:42:11 -0400

    Assignment 3 Completed

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -3,6 +3,7 @@
   Import modules/files you may need to correctly run the script. 
   Make sure to save your DB's uri in the config file, then import it with a require statement!
  */
+
 var fs = require('fs'),
     mongoose = require('mongoose'), 
     Schema = mongoose.Schema, 
@@ -10,12 +11,34 @@ var fs = require('fs'),
     config = require('./config');
 
 /* Connect to your database */
-
+mongoose.connect('mongodb://SWEnghw3:hw3@ds129144.mlab.com:29144/uf_directory_listings');
+var connection = mongoose.connection;
+connection.on("connected", function() {
+  console.log("connected to database");
+})
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
  */
+var listings;
 
+fs.readFile('listings.json', 'utf8', function(err, data) {
+  if (err) throw err;
+  listings = JSON.parse(data);
+  console.log(listings);
+
+  for(var i = 0; i < listings.entries.length; i++) {
+    var listing = new Listing({
+      code: listings.entries[i].code,
+      name: listings.entries[i].name,
+      coordinates: listings.entries[i].coordinates,
+      address: listings.entries[i].address
+    });
+    listing.save(function(err) {
+      if (err) throw err;
+    });
+  };
+});
 
 /* 
   Once you've written + run the script, check out your MongoLab database to ensure that 
--- queries.js
+++ queries.js
@@ -1,10 +1,26 @@
 /* Fill out these functions using Mongoose queries*/
+var fs = require('fs'),
+  mongoose = require('mongoose'), 
+  Schema = mongoose.Schema, 
+  Listing = require('./ListingSchema.js'), 
+  config = require('./config');
 
-var findLibraryWest = function() {
+/* Connect to your database */
+mongoose.connect('mongodb://SWEnghw3:hw3@ds129144.mlab.com:29144/uf_directory_listings');
+var connection = mongoose.connection;
+connection.on("connected", function() {
+console.log("connected to database");
+});
+
+var findLibraryWest = function(err, listings) {
   /* 
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+  Listing.find({name: 'Library West'}, function(err, listing) {
+    if (err) throw err;
+    console.log(listing);
+  });
 };
 var removeCable = function() {
   /*
@@ -12,17 +28,29 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+  Listing.findOneAndRemove({code: 'CABL'}, function(err) {
+    if (err) throw err;
+    console.log('User deleted!');
+  });
 };
 var updatePhelpsMemorial = function() {
   /*
     Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
+  Listing.findOneAndUpdate({code: 'PHL'}, {address: '1275 Center Drive, Gainesville, FL 32611-6131'}, function(err, listing) {
+    if (err) throw err;
+    console.log(listing);
+  });
 };
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+  Listing.find({}, function(err, users) {
+    if (err) throw err;
+    console.log(users);
+  })
 };
 
 findLibraryWest();
